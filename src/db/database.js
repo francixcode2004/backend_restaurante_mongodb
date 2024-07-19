d6d70c5a95490129e98688c8bcba1036ae9d309c
franc <franc@DESKTOP-FOEEVR7>
2024-07-18 22:56:12 -0500

    conexion a mongodb_atlas

--- src/db/database.js
+++ src/db/database.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB conectado a la base de datos');
     } catch (error) {
         console.error('Error al conectar a la base MongoDB:', error);
         process.exit(1);
     }
 };
-
 module.exports = connectDB;
