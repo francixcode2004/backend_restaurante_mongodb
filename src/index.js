bdf253f0c4849708147dd4624986261e2ed002a9
franc <franc@DESKTOP-FOEEVR7>
2024-07-19 03:36:01 -0500

    rutas publicas

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use(require("./routes/user.routes"))
 app.use(require("./routes/menu.routes"));
 app.use(require("./routes/mesero.routes"));
-app.use(require("./routes/user.routes"))
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,10 @@ const usuarioController = require('../controller/userController');
 
 router.post("/api/v1/login",usuarioController.loginUser);
 router.post("/api/v1/register", usuarioController.register);
+router.get("/api/v1",(req,res)=>{res.send("hola mundo")})
 router.use(usuarioController.authenticate);
+
+
 router.put("/api/v1/usuarios/:id", usuarioController.updateUserById);
 
 module.exports = router;
